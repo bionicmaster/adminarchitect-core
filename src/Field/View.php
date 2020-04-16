be05cc9aec7e724e9feb305795def445dafcd694
Larry Mckuydee <larry.mckuydee@gmail.com>
2020-04-16 12:12:37 +0800

    Make view and section view to straight away return template

--- src/Field/View.php
+++ src/Field/View.php
@@ -16,10 +16,6 @@ class View extends Field
     
     protected function template(string $page, string $field = null): string
     {
-        return $this->customTemplate ?? sprintf(
-            'administrator::fields.%s.%s',
-            Str::snake($field ?? class_basename($this)),
-            $page
-        );
+        return $this->customTemplate;
     }
 }
